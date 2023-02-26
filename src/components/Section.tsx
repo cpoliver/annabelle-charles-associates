2ff83550c71d229daf6e7d9fdd80d9db2be2d289
Charlie Oliver <cpoliver@gmail.com>
2023-02-26 17:07:52 +0000

    refactor: use compound component instead of props for Section

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,30 +1,60 @@
 import * as React from "react";
-import { Flex, Stack, Heading, Image } from "@chakra-ui/react";
+import {
+  Flex,
+  Stack,
+  Heading,
+  Image,
+  HeadingProps,
+  ImageProps,
+  FlexProps,
+} from "@chakra-ui/react";
 
-export type SectionProps = React.PropsWithChildren<{
-  heading: string;
-  subheading?: string;
-  image?: string;
-  isFlipped?: boolean;
-}>;
+type Components = {
+  Heading: React.FC<HeadingProps>;
+  Subheading: React.FC<HeadingProps>;
+  Body: React.FC<FlexProps>;
+  Content: React.FC<FlexProps>;
+  Image: React.FC<ImageProps>;
+};
 
-export const Section: React.FC<SectionProps> = ({
-  heading,
-  subheading,
+const Section: React.FC<React.PropsWithChildren> & Components = ({
   children,
-  image,
-  isFlipped,
-}) => (
-  <Stack direction={isFlipped ? "column-reverse" : "column"}>
-    <Heading as="h2">{heading}</Heading>
-    {subheading && <Heading as="h3">{subheading}</Heading>}
-    <Flex gap={4}>
-      <Flex flex={1}>{children}</Flex>
-      {image && (
-        <Flex flex={1}>
-          <Image src={image} />
-        </Flex>
-      )}
-    </Flex>
-  </Stack>
+}) => <Stack>{children}</Stack>;
+
+Section.Heading = (props) => (
+  <Heading
+    as="h2"
+    // size="md"
+    color="gray.900"
+    letterSpacing="wide"
+    fontSize="2.6752rem"
+    textTransform="uppercase"
+    fontWeight="bold"
+    {...props}
+  />
 );
+
+Section.Subheading = (props) => (
+  <Heading
+    as="h3"
+    // size="sm"
+    color="gray.600"
+    letterSpacing="wide"
+    fontSize="1.5rem"
+    textTransform="uppercase"
+    fontWeight="bold"
+    {...props}
+  />
+);
+
+Section.Body = (props) => <Flex flex={1} gap={4} {...props} />;
+
+Section.Content = (props) => <Flex flex={1} {...props} />;
+
+Section.Image = (props) => (
+  <Flex flex={1}>
+    <Image fit="cover" {...props} />
+  </Flex>
+);
+
+export { Section };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,24 @@ import { Section } from "../components/Section";
 
 const IndexPage: React.FC<PageProps> = () => (
   <Layout>
-    <Section
-      heading="Excellent service"
-      subheading="We are an independent recruitment agency that helps employers to connect with exceptional talent at all levels."
-      image="https://www.annabellecharles.co.uk/wp-content/uploads/2020/05/services-img-1.jpg"
-    >
-      <Text>
-        we combine intuition and thoughtful analysis to offer our clients and
-        candidates across the UK with our personalised services. Our beauty and
-        aesthetics recruitment agency uses a thorough recruitment process to
-        bring out a skillset that proves to be invaluable for your business. We
-        know all about the multifaceted industry, from cosmetics, dietary
-        supplements, household products, to other lifestyle essentials.
-      </Text>
+    <Section>
+      <Section.Heading>Excellent service</Section.Heading>
+      <Section.Subheading>
+        We are an independent recruitment agency that helps employers to connect
+        with exceptional talent at all levels.
+      </Section.Subheading>
+      <Section.Body>
+        <Section.Image src="https://www.annabellecharles.co.uk/wp-content/uploads/2020/05/services-img-1.jpg" />
+        <Section.Content>
+          We combine intuition and thoughtful analysis to offer our clients and
+          candidates across the UK with our personalised services. Our beauty
+          and aesthetics recruitment agency uses a thorough recruitment process
+          to bring out a skillset that proves to be invaluable for your
+          business. We know all about the multifaceted industry, from cosmetics,
+          dietary supplements, household products, to other lifestyle
+          essentials.
+        </Section.Content>
+      </Section.Body>
     </Section>
   </Layout>
 );
