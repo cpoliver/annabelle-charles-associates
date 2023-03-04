948b004067220a1ac36eb62afacc6422a8404617
Charlie Oliver <cpoliver@gmail.com>
2023-03-04 14:35:12 +0100

    feat: add Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,34 @@
 import * as React from "react";
+import { Stack, StackProps, Heading, HeadingProps } from "@chakra-ui/react";
 
-export const Hero: React.FC = () => <div>Hero</div>;
+const HeroContainer: React.FC<StackProps> = (props) => <Stack {...props} />;
+
+const HeroHeading: React.FC<HeadingProps> = (props) => (
+  <Heading
+    as="h2"
+    color="gray.900"
+    letterSpacing="wide"
+    fontSize="2.6752rem"
+    textTransform="uppercase"
+    fontWeight="bold"
+    {...props}
+  />
+);
+
+const HeroSubheading: React.FC<HeadingProps> = (props) => (
+  <Heading
+    as="h3"
+    color="gray.600"
+    letterSpacing="wide"
+    fontSize="1.5rem"
+    textTransform="uppercase"
+    fontWeight="bold"
+    {...props}
+  />
+);
+
+export {
+  HeroContainer as Container,
+  HeroHeading as Heading,
+  HeroSubheading as Subheading,
+};
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,14 +2,22 @@ import * as React from "react";
 import { Container, Stack } from "@chakra-ui/react";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
-import { Hero } from "./Hero";
+import * as Hero from "./Hero";
 import { Navigation } from "./Navigation";
 
 export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => (
   <Stack bg="background" h="100vh">
     <Header />
     <Navigation />
-    <Hero />
+    <Hero.Container backgroundImage="https://www.annabellecharles.co.uk/wp-content/uploads/2020/09/banne1-1.jpg">
+      <Hero.Heading>
+        Beauty, fashion & lifestyle recruitment specialists
+      </Hero.Heading>
+      <Hero.Subheading>
+        We are an independent recruitment agency providing tailor-made staffing
+        solutions to SMEs and global businesses across the UK
+      </Hero.Subheading>
+    </Hero.Container>
     <Container maxW="1024px" alignSelf="center">
       <Stack spacing={16}>{children}</Stack>
     </Container>
